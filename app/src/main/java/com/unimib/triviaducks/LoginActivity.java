634231efe3a9871949546f1d83aa61ae44ff660c
albanigiulia <g.albani6@campus.unimib.it>
2024-11-17 18:46:25 +0100

    login updated

--- app/src/main/java/com/unimib/triviaducks/LoginActivity.java
+++ app/src/main/java/com/unimib/triviaducks/LoginActivity.java
@@ -1,6 +1,9 @@
 package com.unimib.triviaducks;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,17 +11,49 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class LoginActivity extends AppCompatActivity {
+import com.google.android.material.snackbar.Snackbar;
+import com.google.android.material.textfield.TextInputEditText;
 
+import org.apache.commons.validator.routines.EmailValidator;
+
+public class LoginActivity extends AppCompatActivity {
+    public static final String TAG = LoginActivity.class.getName();
+
+    private TextInputEditText editTextEMail, editTextPassword;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_login);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        editTextEMail = findViewById(R.id.textInputEmail);
+        editTextPassword = findViewById(R.id.textInputPassword);
+
+        Button loginButton = findViewById(R.id.loginButton);
+
+        loginButton.setOnClickListener(view -> {
+            if(isEmailOk(editTextEMail.getText().toString())) {
+                if (isPasswordOk(editTextPassword.getText().toString())) {
+                    Log.d(TAG, "Launch new activity");
+                    Intent intent = new Intent(this, MainActivity.class);
+                    startActivity(intent);
+                }else{
+                    Log.d(TAG, "Error password");
+                    editTextPassword.setError("Check your password");
+                    Snackbar.make(findViewById(android.R.id.content), "Check your password", Snackbar.LENGTH_SHORT).show();
+
+                }
+            }else{
+                Log.d(TAG, "Error email");
+                editTextEMail.setError("Insert a correct email");
+                Snackbar.make(findViewById(android.R.id.content), "Insert a correct email", Snackbar.LENGTH_SHORT).show();
+            }
         });
+    }
+    private boolean isEmailOk(String email) {
+        return EmailValidator.getInstance().isValid(email);
+    }
+    private boolean isPasswordOk(String password) {
+        return password.length() > 7;
     }
 }
