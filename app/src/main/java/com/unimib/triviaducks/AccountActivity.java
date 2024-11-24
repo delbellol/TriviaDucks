1208b5ef1d69c7c609ab1b127f0a9b7c3c08fd7f
albanigiulia <g.albani6@campus.unimib.it>
2024-11-24 22:38:21 +0100

    AccountActivity updated

--- app/src/main/java/com/unimib/triviaducks/AccountActivity.java
+++ app/src/main/java/com/unimib/triviaducks/AccountActivity.java
@@ -1,26 +1,50 @@
 package com.unimib.triviaducks;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.widget.ImageButton;
 
 import androidx.activity.EdgeToEdge;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class AccountActivity extends AppCompatActivity {
+
+    private ImageButton accountButton;
+
+    // GetContent creates an ActivityResultLauncher<String> to let you pass
+    // in the mime type you want to let the user select
+    ActivityResultLauncher<String> mGetContent = registerForActivityResult(
+            new ActivityResultContracts.GetContent(),
+            new ActivityResultCallback<Uri>() {
+                @Override
+                public void onActivityResult(Uri uri) {
+                    if (uri != null) {
+                        accountButton.setImageURI(uri);
+                    }
+                }
+            }
+    );
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_account);
+
         ImageButton close = findViewById(R.id.close);
+        accountButton = findViewById(R.id.profile);
+
         close.setOnClickListener(view -> {
             Intent intent = new Intent(this, MainActivity.class);
             startActivity(intent);
+        });
+
+        accountButton.setOnClickListener(view -> {
+            mGetContent.launch("image/*");
         });
     }
 }
