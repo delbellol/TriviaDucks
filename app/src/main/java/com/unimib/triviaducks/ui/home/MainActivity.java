4d508d799261308b68a64d0e3f7dbcac0c6beb46
Alessandro Dominoni <aledominoni03@gmail.com>
2024-11-29 17:35:16 +0100

    updated MainFragmennt with home_nav_graph

--- app/src/main/java/com/unimib/triviaducks/ui/home/MainActivity.java
+++ app/src/main/java/com/unimib/triviaducks/ui/home/MainActivity.java
@@ -20,57 +20,5 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-
-        Button oneshot = findViewById(R.id.oneShot);
-        Button trials = findViewById(R.id.trials);
-        ImageButton account = findViewById(R.id.account);
-        ImageButton settings = findViewById(R.id.settings);
-        ImageButton ranking = findViewById(R.id.ranking);
-
-        account.setOnClickListener(view -> {
-            Intent intent = new Intent(this, AccountActivity.class);
-            startActivity(intent);
-        });
-        settings.setOnClickListener(view -> {
-            Intent intent = new Intent(this, SettingsActivity.class);
-            startActivity(intent);
-        });
-        ranking.setOnClickListener(view -> {
-            Intent intent = new Intent(this, RankingActivity.class);
-            startActivity(intent);
-        });
-
-
-        oneshot.setOnClickListener(v -> showDialog("oneshot"));
-        trials.setOnClickListener(v -> showDialog("trials"));
     }
-
-    private void showDialog(String mode) {
-        Dialog dialog = new Dialog(this);
-        dialog.setContentView(R.layout.dialog_game_mode);
-
-        TextView title = dialog.findViewById(R.id.dialog_title);
-        TextView description = dialog.findViewById(R.id.dialog_description);
-        if ("oneshot".equals(mode)) {
-            title.setText(getString(R.string.one_shot));
-            description.setText(getString(R.string.one_shot_description));
-        } else if ("trials".equals(mode)) {
-            title.setText(getString(R.string.trials));
-            description.setText(getString(R.string.trials_description));
-        }
-
-        ImageButton closeButton = dialog.findViewById(R.id.close_dialog);
-        closeButton.setOnClickListener(view -> dialog.dismiss());
-
-        Button play = dialog.findViewById(R.id.play);
-        play.setOnClickListener(view -> {
-            Intent intent = new Intent(this, QuestionActivity.class);
-            intent.putExtra("mode", mode);
-            startActivity(intent);
-            dialog.dismiss();
-        });
-
-        dialog.show();
-    }
-
 }
--- app/src/main/java/com/unimib/triviaducks/ui/home/fragment/MainFragment.java
+++ app/src/main/java/com/unimib/triviaducks/ui/home/fragment/MainFragment.java
@@ -1,14 +1,27 @@
 package com.unimib.triviaducks.ui.home.fragment;
 
+import android.app.Dialog;
+import android.content.Intent;
 import android.os.Bundle;
 
+import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.Navigation;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.TextView;
 
 import com.unimib.triviaducks.R;
+import com.unimib.triviaducks.ui.game.QuestionActivity;
+import com.unimib.triviaducks.ui.home.AccountActivity;
+import com.unimib.triviaducks.ui.home.RankingActivity;
+import com.unimib.triviaducks.ui.home.SettingsActivity;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -17,44 +30,17 @@ import com.unimib.triviaducks.R;
  */
 public class MainFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
     public MainFragment() {
         // Required empty public constructor
     }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment MainFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static MainFragment newInstance(String param1, String param2) {
+    public static MainFragment newInstance() {
         MainFragment fragment = new MainFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
         return fragment;
     }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
     }
 
     @Override
@@ -63,4 +49,60 @@ public class MainFragment extends Fragment {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_main, container, false);
     }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        Button oneshot = view.findViewById(R.id.oneShot);
+        Button trials = view.findViewById(R.id.trials);
+        ImageButton account = view.findViewById(R.id.account);
+        ImageButton settings = view.findViewById(R.id.settings);
+        ImageButton leaderboard = view.findViewById(R.id.ranking);
+
+        account.setOnClickListener(v -> {
+            Navigation.findNavController(v).navigate(R.id.action_mainFragment_to_accountInformationFragment);
+        });
+        settings.setOnClickListener(v -> {
+            Navigation.findNavController(v).navigate(R.id.action_mainFragment_to_settingsFragment);
+        });
+        leaderboard.setOnClickListener(v -> {
+            Navigation.findNavController(v).navigate(R.id.action_mainFragment_to_leaderboardFragment);
+        });
+
+        oneshot.setOnClickListener(v -> {
+            Navigation.findNavController(v).navigate(R.id.action_mainFragment_to_gameModeFragment);
+        });
+        //trials.setOnClickListener(v -> showDialog("trials"));
+    }
+
+/*
+    private void showDialog(String mode) {
+        Dialog dialog = new Dialog(this);
+        dialog.setContentView(R.layout.dialog_game_mode);
+
+        TextView title = dialog.findViewById(R.id.dialog_title);
+        TextView description = dialog.findViewById(R.id.dialog_description);
+        if ("oneshot".equals(mode)) {
+            title.setText(getString(R.string.one_shot));
+            description.setText(getString(R.string.one_shot_description));
+        } else if ("trials".equals(mode)) {
+            title.setText(getString(R.string.trials));
+            description.setText(getString(R.string.trials_description));
+        }
+
+        ImageButton closeButton = dialog.findViewById(R.id.close_dialog);
+        closeButton.setOnClickListener(view -> dialog.dismiss());
+
+        Button play = dialog.findViewById(R.id.play);
+        play.setOnClickListener(view -> {
+            Intent intent = new Intent(this, QuestionActivity.class);
+            intent.putExtra("mode", mode);
+            startActivity(intent);
+            dialog.dismiss();
+        });
+
+        dialog.show();
+    }
+*/
 }
