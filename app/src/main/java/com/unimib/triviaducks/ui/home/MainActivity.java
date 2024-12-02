c252852ba9c06cb8e2bc1fb4e74e8a8d70b4f54d
albanigiulia <g.albani6@campus.unimib.it>
2024-12-02 14:43:41 +0100

    updated mainActivity

--- app/src/main/java/com/unimib/triviaducks/ui/home/MainActivity.java
+++ app/src/main/java/com/unimib/triviaducks/ui/home/MainActivity.java
@@ -9,7 +9,11 @@ import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.fragment.NavHostFragment;
+import androidx.navigation.ui.NavigationUI;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.unimib.triviaducks.R;
 import com.unimib.triviaducks.ui.game.QuestionActivity;
 
@@ -21,57 +25,10 @@ public class MainActivity extends AppCompatActivity {
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
 
-        //Dichiaro i bottoni (R.id.nome_del_bottone dichiarato nel mainfragment layout)
-        //Button oneshot = findViewById(R.id.oneShot);
-        //Button trials = findViewById(R.id.trials);
-        //ImageButton leaderboard = findViewById(R.id.ranking);
-
-        /*collega i bottoni del main ai vari fragment tramite action di home_nav_graph
-        account.setOnClickListener(v -> {
-            Navigation.findNavController(v).navigate(R.id.action_mainFragment_to_accountInformationFragment);
-        });
-
-        settings.setOnClickListener(v -> {
-            Navigation.findNavController(v).navigate(R.id.action_mainFragment_to_settingsFragment);
-        });
-
-        leaderboard.setOnClickListener(v -> {
-            Navigation.findNavController(v).navigate(R.id.action_mainFragment_to_leaderboardFragment);
-        });
-
-        oneshot.setOnClickListener(v -> {
-            Navigation.findNavController(v).navigate(R.id.action_mainFragment_to_gameModeFragment);
-        });
-
-        trials.setOnClickListener(v -> {
-            Navigation.findNavController(v).navigate(R.id.action_mainFragment_to_gameModeFragment);
-        });*/
+        NavHostFragment navHostFragment = (NavHostFragment)getSupportFragmentManager().findFragmentById(R.id.fragmentContainerView);
+        NavController navController = navHostFragment.getNavController();
+        BottomNavigationView bottomNav = findViewById(R.id.bottomNavigation);
+        NavigationUI.setupWithNavController(bottomNav, navController);
     }
-    private void showDialog(String mode) {
-        Dialog dialog = new Dialog(this);
-        //dialog.setContentView(R.layout.dialog_game_mode);
 
-        TextView title = dialog.findViewById(R.id.dialog_title);
-        TextView description = dialog.findViewById(R.id.dialog_description);
-        if ("oneshot".equals(mode)) {
-            title.setText(getString(R.string.one_shot));
-            description.setText(getString(R.string.one_shot_description));
-        } else if ("trials".equals(mode)) {
-            title.setText(getString(R.string.trials));
-            description.setText(getString(R.string.trials_description));
-        }
-
-        ImageButton closeButton = dialog.findViewById(R.id.close_dialog);
-        closeButton.setOnClickListener(view -> dialog.dismiss());
-
-        Button play = dialog.findViewById(R.id.play);
-        play.setOnClickListener(view -> {
-            Intent intent = new Intent(this, QuestionActivity.class);
-            intent.putExtra("mode", mode);
-            startActivity(intent);
-            dialog.dismiss();
-        });
-
-        dialog.show();
-    }
 }
