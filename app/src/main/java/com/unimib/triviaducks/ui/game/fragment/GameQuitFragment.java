d864704984ee114748d1c5271857ac03410836c4
CarraraFabio <f.carrara10@campus.unimib.it>
2024-12-06 09:07:02 +0100

    fixed close button on gameQuitFragment

--- app/src/main/java/com/unimib/triviaducks/ui/game/fragment/GameQuitFragment.java
+++ app/src/main/java/com/unimib/triviaducks/ui/game/fragment/GameQuitFragment.java
@@ -5,14 +5,18 @@ import android.os.Bundle;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.Navigation;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import com.unimib.triviaducks.R;
 
 public class GameQuitFragment extends Fragment {
+
+    Button close;
 
     public GameQuitFragment() {
         // Required empty public constructor
@@ -38,6 +42,14 @@ public class GameQuitFragment extends Fragment {
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+
+        close = view.findViewById(R.id.close);
+
+        close.setOnClickListener(v ->
+                Navigation.findNavController(v).navigate(R.id.action_gameQuitFragment_to_mainActivity)
+        );
+
+
 
     }
 }
