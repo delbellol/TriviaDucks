fccadfc60b35afd066bb47a6c7ff28391ca6773d
Alessandro Dominoni <aledominoni03@gmail.com>
2024-12-16 15:43:52 +0100

    started to create leaderboard fragment

---
+++ app/src/main/java/com/unimib/triviaducks/model/Rank.java
@@ -0,0 +1,31 @@
+package com.unimib.triviaducks.model;
+
+public class Rank {
+    private int position;
+    private int profileImage;
+    private String name;
+    private int score;
+
+    public Rank(int position, int profileImage, String name, int score) {
+        this.position = position;
+        this.profileImage = profileImage;
+        this.name = name;
+        this.score = score;
+    }
+
+    public int getPosition() {
+        return position;
+    }
+
+    public int getProfileImage() {
+        return profileImage;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getScore() {
+        return score;
+    }
+}
--- app/src/main/java/com/unimib/triviaducks/ui/home/fragment/LeaderboardFragment.java
+++ app/src/main/java/com/unimib/triviaducks/ui/home/fragment/LeaderboardFragment.java
@@ -6,6 +6,8 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.Navigation;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
@@ -13,11 +15,19 @@ import android.view.ViewGroup;
 import android.widget.ImageButton;
 
 import com.unimib.triviaducks.R;
+import com.unimib.triviaducks.adapter.RankRecyclerAdapter;
+import com.unimib.triviaducks.model.Rank;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class LeaderboardFragment extends Fragment {
 
+    private RecyclerView recyclerView;
+    private RankRecyclerAdapter adapter;
+    private List<Rank> rankList;
+
     public LeaderboardFragment() {
-        // Required empty public constructor
     }
 
     public static LeaderboardFragment newInstance() {
@@ -33,8 +43,27 @@ public class LeaderboardFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_leaderboard, container, false);
+        View view = inflater.inflate(R.layout.fragment_leaderboard, container, false);
+
+            recyclerView = view.findViewById(R.id.rankList);
+
+            rankList = new ArrayList<>();
+            rankList.add(new Rank(1, R.drawable.p1, "Account 1", 1000));
+            rankList.add(new Rank(2, R.drawable.p2, "Account 2", 950));
+            rankList.add(new Rank(3, R.drawable.p3, "Account 3", 900));
+            rankList.add(new Rank(4, R.drawable.p4, "Account 4", 850));
+            rankList.add(new Rank(5, R.drawable.p5, "Account 5", 800));
+            rankList.add(new Rank(6, R.drawable.p6, "Account 6", 750));
+            rankList.add(new Rank(7, R.drawable.p1, "Account 7", 700));
+            rankList.add(new Rank(8, R.drawable.p2, "Account 8", 650));
+            rankList.add(new Rank(9, R.drawable.p3, "Account 9", 600));
+            rankList.add(new Rank(10, R.drawable.p4, "Account 10", 550));
+
+            recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+            adapter = new RankRecyclerAdapter(rankList);
+            recyclerView.setAdapter(adapter);
+
+            return view;
     }
 
     @Override
